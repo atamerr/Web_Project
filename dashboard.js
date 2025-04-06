af4a34bed1fc4fe4602066893c23dfba714936bc
eyad-404 <eyad2306444@miuegypt.edu.eg>
2025-04-07 00:51:19 +0200

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+document.getElementById('view-products').addEventListener('click', () => {
+  alert("Redirecting to the product page!");
+});
+
+document.getElementById('settings-btn').addEventListener('click', () => {
+  alert("Opening settings...");
+});
